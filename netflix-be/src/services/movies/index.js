5f9089e4124bb156f620354e852bc1a091ff4e54
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-03 23:13:39 +0200

    Post Route with Validators

--- netflix-be/src/services/movies/index.js
+++ netflix-be/src/services/movies/index.js
@@ -33,3 +33,43 @@ moviesRouter.get("/:imdbID", async(req, res, next) => {
     }
 })
 
+// 3. POST:
+moviesRouter.post("/", 
+    [
+        check("Title").exists().withMessage("Must have a Title!"),
+        check("Type").exists().withMessage("You have to write the type of movie!"),
+        check("Year").isLength({min:4, max:4}).withMessage("Write a valid year!"),
+        sanitizeBody("Year").toInt()
+        
+        
+    ],
+    async(req, res, next) => {
+
+        const validationErrors = validationResult(req)
+
+        if(!validationErrors.isEmpty()) {
+            const error = new Error()
+            error.httpStatusCode = 400
+            error.message = validationErrors
+            next(error)
+        } else {
+            try{
+                const newMovies= {
+                    ...req.body,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    id: uniqid()
+                }
+            const movies = await getMovies()
+            movies.push(newMovies)
+            await writeMovies(movies)
+            res.send(newMovies)
+            }
+            catch(error){
+                console.log(error)
+                const err = new Error("Generic Error!")
+                next(err)
+            }
+        }
+   
+})
