4306944af2fe0cc384bc7cb3ed8c5bdc2b545413
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-03 23:13:15 +0200

    Get only One Route

--- netflix-be/src/services/movies/index.js
+++ netflix-be/src/services/movies/index.js
@@ -13,3 +13,23 @@ moviesRouter.get("/", async(req, res, next) => {
            next(err)
         }
    })
+// 2. GET only one:
+moviesRouter.get("/:imdbID", async(req, res, next) => {
+    try{
+        const movies = await getMovies()
+        const movie= movies.find(prod => prod.imdbID === req.params.imdbID)
+
+     if(movies){
+         res.send(movie)
+     } else {
+         const err = new Error()
+         err.httpStatusCode = 404
+         next(err)
+     }
+    } catch(error){
+        console.log(error)
+        const err = new Error("Generic error Happend!")
+        next(err)
+    }
+})
+
